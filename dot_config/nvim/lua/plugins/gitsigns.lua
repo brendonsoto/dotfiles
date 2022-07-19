d3d49725a554d4a632a1a4e77009bea31898af46
brendon <brendon.soto@gmail.com>
2022-07-18 21:41:12 -0400

    updated gitsigns neovim config

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,23 @@
 local gitsigns = require('gitsigns')
-gitsigns.setup {}
 
-local is_wk_present, wk = pcall(require, "which-key")
-if (is_wk_present == false) then
-    print("which-key not found")
-    return
-end
+gitsigns.setup {
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <abbrev_sha>',
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 250,
+    ignore_whitespace = true,
+  },
+  on_attach = function(bufnr)
+    local is_wk_present, wk = pcall(require, "which-key")
+    if (is_wk_present == false) then
+      print("which-key not found")
+      return
+    end
 
--- There's just the name because I'm using the default mappings
--- Without this, hitting just <leader>h will show "+Left" on which-key
-wk.register({
-  name = 'gitsigns',
-}, { prefix = '<leader>h' })
+    wk.register({
+      b = { '<cmd>Gitsigns toggle_current_line_blame<cr>', 'Toggle Git blame' }
+    }, { prefix = '<leader>g' })
+  end
+}
