f3e4d8a4e0b9641946879cc544f7cbf248397af1
brendon <brendon.soto@gmail.com>
2022-07-18 21:41:30 -0400

    added telescope shortcut for creating markdown links

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,11 @@
 local g = vim.g
 local map = vim.api.nvim_set_keymap
 local telescope = require('telescope')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 local builtin = require('telescope.builtin')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
 local previewers = require('telescope.previewers')
 local utils = require('telescope.utils')
 
@@ -32,6 +36,26 @@ telescope.setup {
     }
 }
 
+-- Command for easy making markdown link
+local make_markdown_link = function()
+  require('telescope.builtin').find_files {
+    prompt_title = "Link file",
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        local filename = selection[1]
+        local file_without_dashes = string.gsub(filename, "-", " ")
+        local ext_indx = string.find(filename, "%.")
+        local file_without_ext = string.sub(file_without_dashes, 0, ext_indx-1)
+        local md_link = string.format("[%s](./%s)", file_without_ext, filename)
+        vim.api.nvim_put({ md_link }, "", false, true)
+      end)
+      return true
+    end,
+  }
+end
+
 telescope.load_extension('repo')
 
 
@@ -44,10 +68,12 @@ end
 
 wk.register({
   f = {
+    ["<space>"] = {'<cmd>Telescope builtin<cr>', 'Telescope Builtin'},
     f = {'<cmd>Telescope find_files<cr>', 'Find files'},
     g = {'<cmd>Telescope live_grep<cr>', 'Live grep'},
     b = {'<cmd>Telescope buffers<cr>', 'Find Buffer'},
     h = {'<cmd>Telescope help_tags<cr>', 'Find help tag'},
+    l = {make_markdown_link, 'Make Markdown Link'}
     -- p = { '<cmd>Telescope projects<cr>', 'Find projects' },
     -- ['ts'] = {'<cmd>Telescope treesitter<cr>', 'Telescope Treesitter?'}
   },
