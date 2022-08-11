d429861c088812fa0115b65d5ec094173cecda4d
brendon <brendon@alloy.com>
2022-08-11 16:40:51 -0400

    nvim: trying to make virtual text for diagnostics more useful

--- dot_config/nvim/lua/user/plugins/lsp/configs.lua
+++ dot_config/nvim/lua/user/plugins/lsp/configs.lua
@@ -54,3 +54,26 @@ for _, server in pairs(servers) do
 
   lspconfig[server].setup(opts)
 end
+
+
+-- Extras
+-- Configure more helpful virtual text
+vim.diagnostic.config {
+  virtual_text = false,
+  float = {
+    format = function(diagnostic)
+      if diagnostic.source == 'eslint' then
+        return string.format(
+          '[%s]\n%s',
+          diagnostic.user_data.lsp.code,
+          -- shows the name of the rule
+          diagnostic.message
+        )
+      end
+      return string.format('%s [%s]', diagnostic.message, diagnostic.source)
+    end,
+    severity_sort = true,
+    close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+    max_width = 80,
+  },
+}
