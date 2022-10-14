78c7b74757aac0ebfe3b235d91797acb10e0ddc8
brendon <brendon.soto@gmail.com>
2022-10-14 08:49:07 -0400

    shell: made a function for creating a tmux session using a given dir and name

--- dot_config/shell/tmuxp.sh
+++ dot_config/shell/tmuxp.sh
@@ -1,2 +1,18 @@
 # Launching tmuxp without this results in a prompt asking for this to be added
 export DISABLE_AUTO_TITLE='true'
+
+# @description Create a named tmux session using the default config.
+# @arg $1 string A path to a directory to use as the starting directory.
+# @example
+#   $ mk_tmux_sesh $HOME/Documents
+#   $ Session name? (default is 'default'):
+#   $ my-session
+mk_tmux_sesh() {
+  echo "Session name? (default is 'default'): "
+  read -r input
+
+  name="default"
+  [ "$input" ] && name="$input"
+
+  DIR=$1 SESH_NAME=$name tmuxp load default
+}
