5bfe0c0b75fef3a75b7622c58b8178292693c76d
brendon <brendon@alloy.com>
2022-11-27 21:59:39 -0500

    nvim: swapped vim markdown keymaps

--- dot_config/nvim/lua/user/plugins/vim-markdown.lua
+++ dot_config/nvim/lua/user/plugins/vim-markdown.lua
@@ -1,3 +1,4 @@
+local vks = require('user/utils').vks
 local g = vim.g
 
 g.vim_markdown_folding_disabled = 1 -- no auto-folds plz
@@ -12,6 +13,7 @@ g.vim_markdown_new_list_item_indent = 0 -- no auto-indenting for lists
 -- Command for easy making markdown link
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
+-- FIXME: `cd` into dir if path isn't path of files
 local make_markdown_link = function()
   require('telescope.builtin').find_files {
     prompt_title = "Link file",
@@ -31,19 +33,11 @@ local make_markdown_link = function()
   }
 end
 
-local wk = require('which-key')
-wk.register(
-  {
-    name = 'Markdown',
-    ["<CR>"] = { "<Plug>Markdown_EditUrlUnderCursor", "Edit link under cursor" },
-    ["]]"] = { "<Plug>Markdown_MoveToNextHeader", "Go to next header" },
-    ["[["] = { "<Plug>Markdown_MoveToPreviousHeader", "Go to previous header" },
-    ["<leader>"] = {
-      t = { ":Toc<CR>", "Table of Contents" },
-      ["it"] = { ":InsertToc<CR>", "Insert Table of Contents" },
-      ["ml"] = { make_markdown_link, "Make Markdown Link" },
-    }
-  }, {
-    buffer = vim.api.nvim_get_current_buf()
-  }
-)
+local curr_buf = vim.api.nvim_get_current_buf()
+vks('n', '<CR>', '<Plug>Markdown_EditUrlUnderCursor', { desc = 'Edit link under cursor', buffer = curr_buf })
+vks('n', ']]', '<Plug>Markdown_MoveToNextHeader', { desc = 'Go to next header', buffer = curr_buf })
+vks('n', '[[', '<Plug>Markdown_MoveToPreviousHeader', { desc = 'Go to previous header', buffer = curr_buf })
+
+vks('n', '<leader>t', ':Toc<CR>', { desc = 'Table of Contents', buffer = curr_buf })
+vks('n', '<leader>it', ':InsertToc<CR>', { desc = 'Insert Table of Contents', buffer = curr_buf })
+vks('n', '<leader>ml', make_markdown_link, { desc = 'Make Markdown Link', buffer = curr_buf })
