b9a0f07094fe1208f412fd6353c38499085b6517
brendon <brendon@alloy.com>
2022-11-27 19:30:51 -0500

    nvim: commenting out lsp opts config -- not sure whats wrong

--- dot_config/nvim/lua/user/plugins/lsp/configs.lua
+++ dot_config/nvim/lua/user/plugins/lsp/configs.lua
@@ -22,17 +22,18 @@ require('mason-lspconfig').setup({
 
 -- Setup LSP configs
 for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require('user.plugins.lsp.handlers').on_attach,
-    capabilities = require('user.plugins.lsp.handlers').capabilities,
-  }
+  local opts = {}
+  -- local opts = {
+  --   on_attach = require('user.plugins.lsp.handlers').on_attach,
+  --   capabilities = require('user.plugins.lsp.handlers').capabilities,
+  -- }
 
-  local server_module = 'user.plugins.lsp.settings.' .. server
-  local has_custom_opts, custom_opts = pcall(require, server_module)
-
-  if has_custom_opts then
-    opts = vim.tbl_extend("force", opts, custom_opts)
-  end
+  -- local server_module = 'user.plugins.lsp.settings.' .. server
+  -- local has_custom_opts, custom_opts = pcall(require, server_module)
+  --
+  -- if has_custom_opts then
+  --   opts = vim.tbl_extend("force", opts, custom_opts)
+  -- end
 
   require("lspconfig")[server].setup(opts)
 end
