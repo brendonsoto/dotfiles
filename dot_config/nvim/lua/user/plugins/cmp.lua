38dffd062716151f5290282487fddcbd29fbcaaf
brendon <brendon@alloy.com>
2022-11-28 22:03:21 -0500

    nvim: setup cmp to use buffer source for cmdline

--- dot_config/nvim/lua/user/plugins/cmp.lua
+++ dot_config/nvim/lua/user/plugins/cmp.lua
@@ -40,12 +40,12 @@ cmp.setup({
 })
 
 -- Use buffer source for `/`.
--- cmp.setup.cmdline('/', {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = {
---     { name = 'buffer' }
---   }
--- })
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
 -- Use cmdline & path source for ':'.
 -- cmp.setup.cmdline(':', {
