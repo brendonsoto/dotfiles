6e4770d85c556c6bbd434401e0acb77903b4e2b1
brendon <brendon@alloy.com>
2022-11-29 19:09:50 -0500

    tmuxp: updated script to not ask for name since i rarely rename

--- dot_config/shell/tmuxp.sh
+++ dot_config/shell/tmuxp.sh
@@ -1,19 +1,19 @@
 # Launching tmuxp without this results in a prompt asking for this to be added
 export DISABLE_AUTO_TITLE='true'
 
+# NOTES OF WHAT YOU"VE DONE BEFORE:
+# Passed in arg to rename session outside of dir name.
+# Removed this since I barely renamed.
+
 # @description Create a named tmux session using the default config.
-# Prompts user for session name (uses selected dir as default).
 # @arg $1 string A path to a directory to use as the starting directory.
 # @example
 #   $ mk_tmux_sesh $HOME/Documents
-#   $ Session name? (default is 'default'):
+#   $ Creating session named: Documents
 #   $ my-session
 mk_tmux_sesh() {
   sesh_name=$(basename "$1")
-  echo "Session name? (Will default to '$sesh_name'): "
-  read -r input
-
-  [ "$input" ] && sesh_name="$input"
+  echo "Creating session named: '$sesh_name'"
 
   DIR=$1 SESH_NAME=$sesh_name tmuxp load default
 }
