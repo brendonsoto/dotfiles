d1983414fcb447cd6b45fbf3591ee8794cf8ee94
brendon <brendon@alloy.com>
2022-12-04 20:55:40 -0500

    nvim: forgot to add this when adding lspsaga

--- dot_config/nvim/lua/user/plugins/lsp/handlers.lua
+++ dot_config/nvim/lua/user/plugins/lsp/handlers.lua
@@ -10,10 +10,10 @@ M.on_attach = function(client, bufnr)
   vks('n', 'gd', buf.definition, { desc = 'Go to definition' })
   vks('n', 'gD', buf.declaration, { desc = 'Go to declaration' })
   vks('n', 'gi', buf.implementation, { desc = 'Go to implementation' })
-  vks('n', 'gr', buf.references, { desc = 'See references' })
+  -- vks('n', 'gr', buf.references, { desc = 'See references' })
   vks('n', 'gt', buf.type_definition, { desc = 'Go to type definition' })
-  vks('n', 'K', buf.hover, { desc = 'Hover' })
-  vks('n', '<leader>ca', buf.code_action, { desc = 'Code action' })
+  -- vks('n', 'K', buf.hover, { desc = 'Hover' })
+  -- vks('n', '<leader>ca', buf.code_action, { desc = 'Code action' })
   vks('n', '<leader>fc', buf.formatting, { desc = 'format code' })
 end
 
