6ff0b7095810f058c7e7a8318b1625c2862f3c69
brendon <brendon.soto@gmail.com>
2023-01-02 11:32:07 -0500

    xplr: added keybindings for bookmarking directories

--- dot_config/xplr/init.lua
+++ dot_config/xplr/init.lua
@@ -1247,6 +1247,37 @@ xplr.config.modes.builtin.default = {
           "PopMode",
         },
       },
+      ["m"] = {
+        help = "bookmark directory",
+        messages = {
+          {
+            BashExecSilently0 = [===[
+              PTH="${XPLR_FOCUS_PATH:?}"
+              DIR=$(dirname "$PTH")
+              DIR_ESC=$(printf %q "$DIR")
+              if echo "${DIR_ESC:?}" >> "${XPLR_SESSION_PATH:?}/bookmarks"; then
+                "$XPLR" -m 'LogSuccess: %q' "$DIR_ESC added to bookmarks"
+              else
+                "$XPLR" -m 'LogError: %q' "Failed to bookmark $DIR_ESC"
+              fi
+            ]===],
+          },
+        },
+      },
+      ["`"] = {
+        help = "go to bookmark",
+        messages = {
+          {
+            BashExec0 = [===[
+              PTH=$(cat "${XPLR_SESSION_PATH:?}/bookmarks" | fzf --no-sort)
+              PTH_ESC=$(printf %q "$PTH")
+              if [ "$PTH" ]; then
+                "$XPLR" -m 'FocusPath: %q' "$PTH"
+              fi
+            ]===],
+          },
+        },
+      },
     },
     on_number = {
       help = "input",
