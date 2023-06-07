016c57049040614b5a06aeef54bc2059fc6ddf84
brendon <brendon.soto@gmail.com>
2023-06-06 23:48:27 -0400

    nvim: fixing up lsp + mason setup

--- dot_config/nvim/lua/user/plugins/lsp.lua
+++ dot_config/nvim/lua/user/plugins/lsp.lua
@@ -29,90 +29,93 @@ return {
       'williamboman/mason.nvim',
       'neovim/nvim-lspconfig',
     },
-    opts = {
-      ensure_installed = servers,
-      automatic_installation = true,
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    opts = function()
-      local runtime_path = vim.split(package.path, ';')
-      table.insert(runtime_path, "lua/?.lua")
-      table.insert(runtime_path, "lua/?/init.lua")
-
-      return {
-        eslint = {
-          settings = {
-            filetypes = {
-              "graphql",
-              "javascript",
-              "javascriptreact",
-              "javascript.jsx",
-              "typescript",
-              "typescriptreact",
-              "typescript.tsx",
-              "vue",
-            }
-          }
-        },
-
-        lua_ls = {
-          settings = {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path
-              },
-              diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
-              },
-              workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.stdpath("config") .. "/lua"] = true,
-                }
-              },
-              -- Do not send telemetry data containing a randomized but unique identifier
-              telemetry = {enable = false}
-            }
-          }
-        }
-      }
-    end,
-    config = function()
+    opts = function ()
+      local lspconfig = require('lspconfig')
       local vks = require('user.utils').vks
       local buf = vim.lsp.buf
 
-      -- Setup LSP configs
-      for _, server in pairs(servers) do
-        local opts = {
-          on_attach = function(client, bufnr)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-            vks('n', 'gd', buf.definition, { desc = 'Go to definition' })
-            vks('n', 'gD', buf.declaration, { desc = 'Go to declaration' })
-            vks('n', 'gi', buf.implementation, { desc = 'Go to implementation' })
-            -- vks('n', 'gr', buf.references, { desc = 'See references' })
-            vks('n', 'gt', buf.type_definition, { desc = 'Go to type definition' })
-            -- vks('n', 'K', buf.hover, { desc = 'Hover' })
-            -- vks('n', '<leader>ca', buf.code_action, { desc = 'Code action' })
-            vks('n', '<leader>fc', buf.formatting, { desc = 'format code' })
-          end,
-
-          -- NOTE: Using this instead of cmp for now because cmp's capabilites don't work
-          -- M.capabilities = vim.lsp.protocol.make_client_capabilities()
-          capabilities = require('cmp_nvim_lsp').default_capabilities(),
-        }
-
-        require("lspconfig")[server].setup(opts)
+        vks('n', 'gd', buf.definition, { desc = 'Go to definition' })
+        vks('n', 'gD', buf.declaration, { desc = 'Go to declaration' })
+        vks('n', 'gi', buf.implementation, { desc = 'Go to implementation' })
+        -- vks('n', 'gr', buf.references, { desc = 'See references' })
+        vks('n', 'gt', buf.type_definition, { desc = 'Go to type definition' })
+        -- vks('n', 'K', buf.hover, { desc = 'Hover' })
+        -- vks('n', '<leader>ca', buf.code_action, { desc = 'Code action' })
+        vks('n', '<leader>fc', buf.formatting, { desc = 'format code' })
       end
 
+      -- NOTE: Using this instead of cmp for now because cmp's capabilites don't work
+      -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      return {
+        ensure_installed = servers,
+        automatic_installation = true,
+        handlers = {
+          -- The default handler
+          -- Will be called for each installed server that doesn't have
+          -- a dedicated handler.
+          function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+            })
+          end,
+          -- Specific servers
+          ["lua_ls"] = function ()
+            lspconfig.lua_ls.setup {
+              on_attach = on_attach,
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                  },
+                  diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = {'vim'}
+                  },
+                  workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                  },
+                  -- Do not send telemetry data containing a randomized but unique identifier
+                  telemetry = {enable = false}
+                }
+              }
+            }
+          end,
+
+          ["eslint"] = function ()
+            lspconfig.eslint.setup {
+              on_attach = on_attach,
+              capabilities = capabilities,
+              settings = {
+                filetypes = {
+                  "graphql",
+                  "javascript",
+                  "javascriptreact",
+                  "javascript.jsx",
+                  "typescript",
+                  "typescriptreact",
+                  "typescript.tsx",
+                  "vue",
+                }
+              }
+            }
+          end,
+        },
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
       -- Extras
       -- Configure more helpful virtual text
       vim.diagnostic.config {
