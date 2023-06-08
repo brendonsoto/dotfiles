61bca0cb61d021c27eb6fec0025931e918ae4c24
brendon soto <brendon@alloy.com>
2023-06-08 11:10:46 -0400

    shell: updated git aliases

--- dot_config/shell/git.sh
+++ dot_config/shell/git.sh
@@ -1,7 +1,8 @@
 alias gbr="git branch --sort=-committerdate | head -5"
 alias gf="git fetch"
 alias gfarm="gcm; git fetch && git rebase origin/main"
-alias gp="git push"
+alias gp="git pull"
+alias gP="git push"
 alias gm="git merge"
 alias grh="git reset --hard"
 alias grm="git rebase main"
@@ -10,6 +11,7 @@ alias gwa="git worktree add"
 alias gwl="git worktree list"
 alias gwr="git worktree remove"
 alias lg="lazygit"
+alias gu="gitui"
 
 gcm() {
   branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
