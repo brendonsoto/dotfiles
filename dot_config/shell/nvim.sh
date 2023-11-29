28612efc784b7c574464a0008ba849b0bfcf6a7d
brendon <brendon.soto@gmail.com>
2023-11-29 00:45:17 -0500

    update the VISUAL env var to nvim/vim

--- dot_config/shell/nvim.sh
+++ dot_config/shell/nvim.sh
@@ -2,9 +2,11 @@
 if [ -n "$(command -v nvim)" ]
 then
   export EDITOR=nvim
+  export VISUAL=nvim
   alias v="nvim"
 else
   echo "neovim not installed!"
   export EDITOR=vim
+  export VISUAL=vim
   alias v="vim"
 fi
