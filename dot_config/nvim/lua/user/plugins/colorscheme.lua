342b3b05d745c82ce1d3b7c07d292bb280bff56a
brendon <brendon.soto@gmail.com>
2024-03-07 11:21:00 -0500

    update wezterm and neovim to use tokyo night as colorscheme
    Trying this out since it seems to have better contrast

--- dot_config/nvim/lua/user/plugins/colorscheme.lua
+++ dot_config/nvim/lua/user/plugins/colorscheme.lua
@@ -5,24 +5,23 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require('tokyonight').setup({
-        style = 'storm',
-        -- I like transparency, but only when I can have my editor in one window
-        -- and switching windows is immediate, no animation or delay. The
-        -- animation makes me a bit nauseaous and I haven't made the time to
-        -- look into reducing motion on MacOS & Linux.
-        -- transparent = true,
-        -- styles = {
-        --   markup = {
-        --     italic = {
-        --       markdown_inline = {
-        --         italic = true
-        --       }
-        --     }
-        --   }
-        -- }
-      })
-      vim.cmd([[colorscheme tokyonight]])
+      -- require('tokyonight').setup({
+      --   -- I like transparency, but only when I can have my editor in one window
+      --   -- and switching windows is immediate, no animation or delay. The
+      --   -- animation makes me a bit nauseaous and I haven't made the time to
+      --   -- look into reducing motion on MacOS & Linux.
+      --   -- transparent = true,
+      --   -- styles = {
+      --   --   markup = {
+      --   --     italic = {
+      --   --       markdown_inline = {
+      --   --         italic = true
+      --   --       }
+      --   --     }
+      --   --   }
+      --   -- }
+      -- })
+      vim.cmd([[colorscheme tokyonight-night]])
 
       -- Enable italics in markdown files
       vim.api.nvim_set_hl(0, '@markup.italic.markdown_inline', { link = '@markup.emphasis' })
