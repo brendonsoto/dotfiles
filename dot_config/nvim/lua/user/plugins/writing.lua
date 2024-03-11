edec35dd2857ddd3778d19aae4bd5e3a5576313a
brendon <brendon.soto@gmail.com>
2024-03-10 23:07:50 -0400

    (nvim) remove obsidian plugin and update change header keymaps

--- dot_config/nvim/lua/user/plugins/writing.lua
+++ dot_config/nvim/lua/user/plugins/writing.lua
@@ -1,79 +1,4 @@
 return {
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    -- ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-      "BufReadPre " .. vim.fn.expand "~" .. "/Documents/knowledge-notes/**.md",
-      "BufNewFile " .. vim.fn.expand "~" .. "/Documents/knowledge-notes/**.md",
-    },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-      'hrsh7th/nvim-cmp',
-      'nvim-telescope/telescope.nvim',
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "knowledge-notes",
-          path = "~/Documents/knowledge-notes",
-        },
-      },
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-        new_notes_location = 'current_dir',
-      },
-      mappings = {
-        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ["gf"] = {
-          action = function()
-            return require("obsidian").util.gf_passthrough()
-          end,
-          opts = { noremap = false, expr = true, buffer = true },
-        },
-        -- Toggle check-boxes.
-        ["<leader>ch"] = {
-          action = function()
-            return require("obsidian").util.toggle_checkbox()
-          end,
-          opts = { buffer = true },
-        },
-      },
-      disable_frontmatter = false,
-      -- -- Optional, alternatively you can customize the frontmatter data.
-      -- note_frontmatter_func = function(note)
-      --   -- This is equivalent to the default frontmatter function.
-      --   local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      --   -- `note.metadata` contains any manually added fields in the frontmatter.
-      --   -- So here we just make sure those fields are kept in the frontmatter.
-      --   if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-      --     for k, v in pairs(note.metadata) do
-      --       out[k] = v
-      --     end
-      --   end
-      --   return out
-      -- end,
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M",
-        -- A map for custom variables, the key should be the variable and the value a function
-        substitutions = {},
-      },
-      follow_url_func = function(url)
-        -- Open the URL in the default web browser.
-        -- vim.fn.jobstart({"open", url})  -- Mac OS
-        vim.fn.jobstart({ "xdg-open", url }) -- linux
-      end,
-    }
-  },
-
   -- Markdown
   {
     'preservim/vim-markdown',
@@ -117,8 +42,8 @@ return {
 
       local curr_buf = vim.api.nvim_get_current_buf()
       vks('n', '<CR>', '<Plug>Markdown_EditUrlUnderCursor', { desc = 'Edit link under cursor', buffer = curr_buf })
-      vks('n', ']]', '<Plug>Markdown_MoveToNextHeader', { desc = 'Go to next header', buffer = curr_buf })
-      vks('n', '[[', '<Plug>Markdown_MoveToPreviousHeader', { desc = 'Go to previous header', buffer = curr_buf })
+      vks('n', ']h', '<Plug>Markdown_MoveToNextHeader', { desc = 'Go to next header', buffer = curr_buf })
+      vks('n', '[h', '<Plug>Markdown_MoveToPreviousHeader', { desc = 'Go to previous header', buffer = curr_buf })
 
       vks('n', '<leader>t', ':Toc<CR>', { desc = 'Table of Contents', buffer = curr_buf })
       vks('n', '<leader>it', ':InsertToc<CR>', { desc = 'Insert Table of Contents', buffer = curr_buf })
