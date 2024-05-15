b8d8e4ab4879c5a44632012eb578e769d4d48500
brendon <brendon.soto@gmail.com>
2024-05-15 16:59:37 -0400

    (nvim) moved treesitter plugin config to its own file

--- dot_config/nvim/lua/user/plugins/treesitter.lua
+++ dot_config/nvim/lua/user/plugins/treesitter.lua
@@ -10,55 +10,5 @@ return {
     'nvim-treesitter/nvim-treesitter',
     version = false,
     build = ':TSUpdate',
-    -- event = { "BufReadPost", "BufNewFile" },
-    opts = function()
-      return {
-        autotag = {
-          enable = true,
-        },
-        ensure_installed = {
-          "c",
-          "css",
-          "dockerfile",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "norg",
-          "python",
-          "query",
-          "regex",
-          "ruby",
-          "rust",
-          "scss",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-        },
-        highlight = {
-          enable = true,
-          custom_captures = { NOTE = "Special" },
-          -- additional_vim_regex_highlighting = { "typescriptreact" }
-          additional_vim_regex_highlighting = false,
-        },
-        rainbow = {
-          enable = true,
-          extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-          max_file_lines = nil -- Do not enable for files with more than n lines, int
-          -- colors = {}, -- table of hex strings
-          -- termcolors = {} -- table of colour name strings
-        },
-        incremental_selection = { enable = true },
-        indent = { enable = true },
-        textobjects = { enable = true }
-      }
-    end,
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
   },
 }
---
+++ dot_config/nvim/plugin/treesitter.lua
@@ -0,0 +1,47 @@
+require('nvim-treesitter.configs').setup({
+  autotag = {
+    enable = true,
+  },
+  ensure_installed = {
+    "c",
+    "css",
+    "dockerfile",
+    "html",
+    "javascript",
+    "jsdoc",
+    "json",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "norg",
+    "python",
+    "query",
+    "regex",
+    "ruby",
+    "rust",
+    "scss",
+    "tsx",
+    "typescript",
+    "vim",
+    "vimdoc",
+  },
+  sync_install = false,
+  auto_install = false,
+  ignore_install = {},
+  highlight = {
+    enable = true,
+    custom_captures = { NOTE = "Special" },
+    -- additional_vim_regex_highlighting = { "typescriptreact" }
+    additional_vim_regex_highlighting = false,
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+  incremental_selection = { enable = true },
+  indent = { enable = true },
+  textobjects = { enable = true }
+})
