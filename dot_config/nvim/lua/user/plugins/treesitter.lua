7a6e49873176f5b00edd67f8da402fa48ebcec16
brendon <brendon.soto@gmail.com>
2025-01-26 22:09:07 -0500

    explicitly state options for nvim-ts-autotag
    The plugin updated but no longer uses a table with defaults set up when
    calling the `setup` function

--- dot_config/nvim/lua/user/plugins/treesitter.lua
+++ dot_config/nvim/lua/user/plugins/treesitter.lua
@@ -5,7 +5,14 @@ return {
       enable_autocmd = false,
     },
   },
-  { 'windwp/nvim-ts-autotag' },
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false,
+    },
+  },
   {
     'nvim-treesitter/nvim-treesitter',
     version = false,
